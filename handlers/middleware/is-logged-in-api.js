347e653eaadd5b35f92cd29765e2d4a791408728
Eirik Vullum <evullum@gmail.com>
2014-10-16 22:32:37 +0200

    replace deprecated res.send(code, data)

--- handlers/middleware/is-logged-in-api.js
+++ handlers/middleware/is-logged-in-api.js
@@ -1,6 +1,6 @@
 exports = module.exports = function() {
     return function(req, res, next) {
-        if (!req.isAuthenticated()) return res.send(401, {
+        if (!req.isAuthenticated()) return res.status(401).send({
             error: 'not authorized'
         });
         return next();
