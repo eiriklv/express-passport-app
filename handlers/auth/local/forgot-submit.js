7051f3d5eef0f42dc38c3681464247514ed2d888
P. Mark Anderson <manderson@flightstats.com>
2015-04-13 15:54:32 -0700

    WIP: started json tests with forgot-submit

--- handlers/auth/local/forgot-submit.js
+++ handlers/auth/local/forgot-submit.js
@@ -1,18 +1,30 @@
 exports = module.exports = function(profile) {
     return function(req, res, next) {
-        profile.forgot(req, function (err, user) {
-            if (err) {
-                req.flash('errorMessage', err.message);
-            }
-            else {
-                req.flash('forgotMessage', 'Please check your email for further directions.');
 
-                if (process.env.NODE_ENV === 'development') {
-                    process.user = user.dataValues;
+        profile.forgot(req, function (err, user) {
+            if (req.accepts('json')) {
+                // treat as an API request
+                if (err) {
+                    res.send(400, { error: err });
+                }
+                else {
+                    res.send(200, { token: user.resetPasswordToken });
                 }
             }
+            else {
+                if (err) {
+                    req.flash('errorMessage', err.message);
+                }
+                else {
+                    req.flash('forgotMessage', 'Please check your email for further directions.');
 
-            res.redirect('/');
+                    if (process.env.NODE_ENV === 'development') {
+                        process.user = user.dataValues;
+                    }
+                }
+
+                res.redirect('/');
+            }
         });
     };
 };
---
+++ test/services/auth/json.js
@@ -0,0 +1,120 @@
+var expect = require('chai').expect;
+var rp = require('request-promise');
+var app = require('../../../app');
+
+exports = module.exports = function () {
+  describe('Local', function() {
+
+    describe('forgot, reset, login, logout', function() {
+      it('should do all the things', function(done) {
+        var forgotOptions = {
+          uri: 'http://localhost:3000/auth/local/forgot',
+          headers: { 'Accept': 'application/json' },
+          method: 'POST',
+          form: {
+            email: 'john@doe.com'
+          }
+        };
+
+        rp(forgotOptions)
+          .catch(function (err) {
+            expect(err.response.headers.location).to.eq('/');
+            return err;
+          })
+          .then(function (argument) {
+            var resetOptions = {
+              uri: 'http://localhost:3000/auth/local/reset',
+              method: 'POST',
+              form: {
+                token: process.user.resetPasswordToken,
+                new_password: 'aNewerPassword',
+                new_password_confirm: 'aNewerPassword'
+              }
+            };
+
+            return rp(resetOptions);
+          })
+          .catch(function (err) {
+            expect(err.response.headers.location).to.eq('/auth/local/login');
+            return err;
+          })
+          .then(function (argument) {
+            var loginOptions = {
+              uri: 'http://localhost:3000/auth/local/login',
+              method: 'POST',
+              form: {
+                email: 'john@doe.com',
+                password: 'aNewerPassword'
+              }
+            };
+
+            return rp(loginOptions);
+          })
+          .catch(function (err) {
+            expect(err.response.headers.location).to.eq('/');
+            return err;
+          })
+          .then(function (argument) {
+            var logoutOptions = {
+              uri: 'http://localhost:3000/auth/local/logout',
+              method: 'GET',
+              form: {
+                email: 'john@doe.com',
+                password: 'aNewerPassword'
+              }
+            };
+
+            return rp(logoutOptions);
+          })
+          .then(function (argument) {
+            expect(argument.indexOf('<html>')).to.not.eq(-1);
+            done();
+          });
+      });
+    });
+
+    describe('forgot, reset, login, logout', function() {
+      it('should do all the things', function(done) {
+        var signupOptions = {
+          uri: 'http://localhost:3000/auth/local/signup',
+          method: 'POST',
+          form: {
+            email: 'bob@dole.com',
+            password: '123456',
+            password_confirm: '123456'
+          }
+        };
+
+        rp(signupOptions)
+          .catch(function (err) {
+            expect(err.response.headers.location).to.eq('/');
+            return err;
+          })
+          .then(function (argument) {
+            var verifyOptions = {
+              uri: 'http://localhost:3000/auth/local/verify?' + process.token,
+              method: 'GET'
+            };
+
+            return rp(verifyOptions);
+          })
+          .then(function (body) {
+            expect(body.indexOf('Home')).to.not.eq(-1);
+            return body;
+          })
+          .then(function (argument) {
+            var unlinkOptions = {
+              uri: 'http://localhost:3000/auth/local/unlink',
+              method: 'GET'
+            };
+
+            return rp(unlinkOptions);
+          })
+          .then(function (body) {
+            expect(body.indexOf('Home')).to.not.eq(-1);
+            done();
+          })
+      });
+    });  
+  });
+};
